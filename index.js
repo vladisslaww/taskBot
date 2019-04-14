711b4db54e279118a06a114c3febbb686d071e16
Vlad <vladisslaww@gmail.com>
2019-04-14 11:34:50 +0300

    bot.start fix

--- index.js
+++ index.js
@@ -123,6 +123,8 @@ async function addTask (ctx) {
       trello.addCard(task, null, user.listID)
         .then(res => {
           ctx.reply('The task has been added');
+          trello.addMemberToCard(res.data.id, user.trelloID)
+            .catch( e => console.log(e) );
         })
 
     })
